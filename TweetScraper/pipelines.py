b79a6f4c5af7d3953505efce7b6a15f117e14334
maziyank <maziyank@gmail.com>
2021-07-18 06:44:47 +0700

    simplified

--- TweetScraper/pipelines.py
+++ TweetScraper/pipelines.py
@@ -3,54 +3,27 @@ from scrapy.utils.project import get_project_settings
 
 from TweetScraper.items import Tweet, User
 from TweetScraper.utils import mkdirs
+from elasticsearch import Elasticsearch
 
 
 logger = logging.getLogger(__name__)
 SETTINGS = get_project_settings()
 
-class SaveToFilePipeline(object):
-    ''' pipeline that save data to disk '''
+# For Future Used
+class SaveToElasticPipeline(object):
+    ''' pipeline that save data to elasticdb '''
 
     def __init__(self):
-        self.saveTweetPath = SETTINGS['SAVE_TWEET_PATH']
-        self.saveUserPath = SETTINGS['SAVE_USER_PATH']
-        mkdirs(self.saveTweetPath) # ensure the path exists
-        mkdirs(self.saveUserPath)
-
+        self.es = Elasticsearch(port=SETTINGS['ELASTIC_PORT'])
 
     def process_item(self, item, spider):
-        if isinstance(item, Tweet):
-            savePath = os.path.join(self.saveTweetPath, item['id_'])
-            if os.path.isfile(savePath):
-                pass # simply skip existing items
-                # logger.debug("skip tweet:%s"%item['id_'])
-                ### or you can rewrite the file, if you don't want to skip:
-                # self.save_to_file(item,savePath)
-                # logger.debug("Update tweet:%s"%item['id_'])
-            else:
-                self.save_to_file(item,savePath)
-                logger.debug("Add tweet:%s" %item['id_'])
+        if isinstance(item, Tweet):             
+            self.es.index(index=SETTINGS['ELASTIC_TWEET_COLLECTION'], id=item['id_'], body=dict(item))
+            logger.debug("Add tweet:%s" %item['id_'])
 
-        elif isinstance(item, User):
-            savePath = os.path.join(self.saveUserPath, item['id_'])
-            if os.path.isfile(savePath):
-                pass # simply skip existing items
-                # logger.debug("skip user:%s"%item['id_'])
-                ### or you can rewrite the file, if you don't want to skip:
-                # self.save_to_file(item,savePath)
-                # logger.debug("Update user:%s"%item['id_'])
-            else:
-                self.save_to_file(item, savePath)
-                logger.debug("Add user:%s" %item['id_'])
+        elif isinstance(item, User):                        
+            self.es.index(index=SETTINGS['ELASTIC_USER_COLLECTION'], id=item['id_'], body=dict(item))
+            logger.debug("Add user:%s" %item['id_'])
 
         else:
-            logger.info("Item type is not recognized! type = %s" %type(item))
-
-
-    def save_to_file(self, item, fname):
-        ''' input: 
-                item - a dict like object
-                fname - where to save
-        '''
-        with open(fname, 'w', encoding='utf-8') as f:
-            json.dump(dict(item), f, ensure_ascii=False)
+            logger.info("Item type is not recognized! type = %s" %type(item))            
--- TweetScraper/settings.py
+++ TweetScraper/settings.py
@@ -1,5 +1,5 @@
 # !!! # Crawl responsibly by identifying yourself (and your website/e-mail) on the user-agent
-USER_AGENT = 'TweetScraper'
+USER_AGENT = 'xxx@xxx.id'
 
 # settings for spiders
 BOT_NAME = 'TweetScraper'
@@ -8,21 +8,23 @@ LOG_LEVEL = 'INFO'
 SPIDER_MODULES = ['TweetScraper.spiders']
 NEWSPIDER_MODULE = 'TweetScraper.spiders'
 ITEM_PIPELINES = {
-    'TweetScraper.pipelines.SaveToFilePipeline':100,
+    'crawlab.pipelines.CrawlabMongoPipeline': 888,
+    # 'TweetScraper.pipelines.SaveToElasticPipeline':100,
 }
-
-# settings for where to save data on disk
-SAVE_TWEET_PATH = './Data/tweet/'
-SAVE_USER_PATH = './Data/user/'
 
 DOWNLOAD_DELAY = 1.0
 
 # settings for selenium
 from shutil import which
-SELENIUM_DRIVER_NAME = 'firefox'
-SELENIUM_BROWSER_EXECUTABLE_PATH = which('firefox')
-SELENIUM_DRIVER_EXECUTABLE_PATH = which('geckodriver')
+SELENIUM_DRIVER_NAME = 'chrome'
+SELENIUM_BROWSER_EXECUTABLE_PATH = which('chrome')
+SELENIUM_DRIVER_EXECUTABLE_PATH = which('chromedriver')
 SELENIUM_DRIVER_ARGUMENTS=['-headless']  # '--headless' if using chrome instead of firefox
 DOWNLOADER_MIDDLEWARES = {
     'scrapy_selenium.SeleniumMiddleware': 800
 }
+
+# elastic
+ELASTIC_TWEET_COLLECTION = 'tweet'
+ELASTIC_USER_COLLECTION = 'user'
+ELASTIC_PORT = 9200
